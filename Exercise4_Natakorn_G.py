dba7c4e920ac1a749259e057b103044a59442ed7
lomaty <lomaty124@hotmail.com>
2021-09-28 15:52:43 +0700

    ทดสอบใช้ตัวแปร

---
+++ Exercise4_Natakorn_G.py
@@ -0,0 +1,15 @@
+'''
+FE = Foundation English
+GB = General Business
+ICS = Introduction to Computer Systems
+CP = Computer Programming
+'''
+FE = 90
+GB = 85
+ICS = 70.5
+CP = 95.5
+print("--- Your Score ---")
+print("Foundation English                 ",": ", FE)
+print("General Business                   ",": ", GB)
+print("Introduction to Computer Systems   ",": ", ICS)
+print("Computer Programming               ",": ", CP)
