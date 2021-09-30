657e46193dbdbb50b2b87308783b44b365190cae
lomaty <lomaty124@hotmail.com>
2021-09-30 16:17:23 +0700

    Exercise5.1

---
+++ Exercise5/Exercise5_1_Natakorn_G.py
@@ -0,0 +1,11 @@
+x = int(input("1st Number : "))
+y = int(input("2nd Number : "))
+plus = x+y
+minus = x-y
+multiply = x*y
+divide = x/y
+print("---Result---")
+print(x,"+",y,"=",plus)
+print(x,"-",y,"=",minus)
+print(x,"*",y,"=",multiply)
+print(x,"/",y,"=",divide)
